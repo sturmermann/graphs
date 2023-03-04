9fa4a22f0769c220246a48cd315de99ca4e31151
Nestermann <sturmer.mann@gmail.com>
2023-03-04 23:05:30 +0300

    v0.19

--- main.py
+++ main.py
@@ -13,15 +13,21 @@ def args():
     parser.add_argument('filename', help = "Filename, nothing more")
 
 def preprocessing(img):
-    blur = cv.GaussianBlur(img,(5,5),0)
-    ret,imgPd = cv.threshold(blur,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)    
+    #alpha = 1 # Contrast control (1.0-3.0)
+    #beta = 0 # Brightness control (0-100)
+    #img = cv.convertScaleAbs(img, alpha=alpha, beta=beta)
+    #cv.imwrite("imgFirst.jpg", img)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.resize(img, (1020, 980))
+    img = cv.GaussianBlur(img,(21,21),0)
+    imgPd = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
     cv.imwrite("imgPd.jpg", imgPd)
     return imgPd
 
 def segmentation(img):
-    kernel = np.ones((5,5),np.uint8)
-    segmentatedNode = cv.erode(img,kernel,iterations = 10)
-    segmentatedNode = cv.dilate(segmentatedNode,kernel,iterations =10)
+    kernel = np.ones((3,3),np.uint8)
+    #segmentatedNode = cv.erode(img,kernel,iterations = 5)
+    segmentatedNode = cv.dilate(img,kernel,iterations = 1)
     cv.imwrite("segmentatedNode.jpg", segmentatedNode)
     contours, hierarchy = cv.findContours(segmentatedNode, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     #simple = cv.cvtColor(segmentatedNode, cv.COLOR_BGR2RGB)
@@ -36,9 +42,11 @@ def segmentation(img):
         center = (int(x), int(y))
         radius = int(radius)
         nodes.append([center, radius])       
-        cv.circle(segmentatedEdge, center, radius-2, (0,0,0), -1)
+        cv.circle(segmentatedNode, center, radius, (255,255,255), -1)
+        cv.circle(segmentatedEdge, center, radius+5, (0,0,0), -1)
         cv.putText(segmentatedEdge, str(num), center, cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         num+=1
+    cv.imwrite("segmentatedNode2.jpg", segmentatedNode)
     print(nodes)
     cv.imwrite("segmentatedEdge.png", segmentatedEdge)
     matrix = []
@@ -49,11 +57,11 @@ def segmentation(img):
     #print(matrix)
     lines = cv.HoughLinesP(
             segmentatedEdge, # Input edge image
-            1, # Distance resolution in pixels
-            np.pi/180, # Angle resolution in radians
+            2, # Distance resolution in pixels
+            np.pi/360, # Angle resolution in radians
             threshold=100, # Min number of votes for valid line
-            minLineLength=10, # Min allowed length of line
-            maxLineGap=10 # Max allowed gap between line for joining them
+            minLineLength=25, # Min allowed length of line
+            maxLineGap=50 # Max allowed gap between line for joining them
             )
     edges = []
     for points in lines:
@@ -74,7 +82,7 @@ def segmentation(img):
             start_x = edge[0][0] - center_x
             start_y = edge[0][1] - center_y
             #print(i, center_x, center_y, edge[0][0], edge[0][1], start_x, start_y, start_x*start_x + start_y*start_y, nodes[i][1])
-            if start_x*start_x + start_y*start_y <= (nodes[i][1]+10)*(nodes[i][1]+10):
+            if start_x**2 + start_y**2 <= (nodes[i][1]+30)**2:
                 startPoint = i
                 break
         for i in range(0, len(nodes)):
@@ -82,9 +90,12 @@ def segmentation(img):
             center_y = nodes[i][0][1]
             end_x = edge[1][0] - center_x
             end_y = edge[1][1] - center_y
-            if end_x*end_x + end_y*end_y <= (nodes[i][1]+10)*(nodes[i][1]+10):
+            if end_x**2 + end_y**2 <= (nodes[i][1]+30)**2:
                 endPoint = i
                 break
+        #print(endPoint, startPoint)
+        # if endPoint == 0 or startPoint == 0:
+        #     cv.line(segmentatedEdge,(edge[0][0], edge[0][1]),(edge[1][0],edge[][1]),(0,255,0),2)
         if endPoint == -1 or startPoint == -1:
             continue
         matrix[startPoint][endPoint] = 1
@@ -96,11 +107,9 @@ def main():
     args = parser.parse_args()
     filename = args.filename
     image = cv.imread(filename)
-    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     if image is None:
         print("Error: no such file or directory")
         return -1
-    image = cv.resize(image, (1020, 980))
     image = preprocessing(image)
     image = segmentation(image)
     
