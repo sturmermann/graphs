4510708acd5e526083a531b8d204b98b9cbc5611
Nestermann <93948212+sturmermann@users.noreply.github.com>
2023-03-04 11:45:16 +0300

    v0.2
    Now we can say coordinates of nodes & separate on image nodes from edges

--- main.py
+++ main.py
@@ -18,16 +18,39 @@ def preprocessing(img):
     cv.imwrite("imgPd.jpg", imgPd)
     return imgPd
 
+def segmentation(img):
+    kernel = np.ones((5,5),np.uint8)
+    segmentatedNode = cv.erode(img,kernel,iterations = 10)
+    segmentatedNode = cv.dilate(segmentatedNode,kernel,iterations =10)
+    cv.imwrite("segmentatedNode.jpg", segmentatedNode)
+    contours, hierarchy = cv.findContours(segmentatedNode, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    #simple = cv.cvtColor(segmentatedNode, cv.COLOR_BGR2RGB)
+    #cv.drawContours(simple, contours, -1, (10,0,255), 5)
+    #cv.imwrite("segmentatedNodeCounters.jpg", simple)
+    nodes = []
+    #segmentatedNode = cv.cvtColor(segmentatedNode, cv.COLOR_BGR2RGB)
+    segmentatedEdge = img
+    for cnt in contours:
+        (x,y),radius = cv.minEnclosingCircle(cnt)
+        center = (int(x), int(y))
+        radius = int(radius)
+        nodes.append([center, radius])       
+        cv.circle(segmentatedEdge, center, radius, (0,0,0), -1)
+    print(nodes)
+    cv.imwrite("segmentatedEdge.png", segmentatedEdge)
+
 def main():
     global parser
     args = parser.parse_args()
     filename = args.filename
-    image = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+    image = cv.imread(filename)
+    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     if image is None:
         print("Error: no such file or directory")
         return -1
-    #image = cv.resize(image, (1980, 1080))
+    image = cv.resize(image, (1020, 980))
     image = preprocessing(image)
+    image = segmentation(image)
     
     
 
