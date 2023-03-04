688b938f18e1a280ee7f5d0e674924c120aad69b
Nestermann <sturmer.mann@gmail.com>
2023-03-04 17:27:25 +0300

    first success

--- main.py
+++ main.py
@@ -30,14 +30,66 @@ def segmentation(img):
     nodes = []
     #segmentatedNode = cv.cvtColor(segmentatedNode, cv.COLOR_BGR2RGB)
     segmentatedEdge = img
+    num = 0
     for cnt in contours:
         (x,y),radius = cv.minEnclosingCircle(cnt)
         center = (int(x), int(y))
         radius = int(radius)
         nodes.append([center, radius])       
-        cv.circle(segmentatedEdge, center, radius, (0,0,0), -1)
+        cv.circle(segmentatedEdge, center, radius-2, (0,0,0), -1)
+        cv.putText(segmentatedEdge, str(num), center, cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        num+=1
     print(nodes)
     cv.imwrite("segmentatedEdge.png", segmentatedEdge)
+    matrix = []
+    for i in range(len(nodes)):
+        matrix.append([])
+        for j in range(len(nodes)):
+            matrix[i].append(0)
+    #print(matrix)
+    lines = cv.HoughLinesP(
+            segmentatedEdge, # Input edge image
+            1, # Distance resolution in pixels
+            np.pi/180, # Angle resolution in radians
+            threshold=100, # Min number of votes for valid line
+            minLineLength=10, # Min allowed length of line
+            maxLineGap=10 # Max allowed gap between line for joining them
+            )
+    edges = []
+    for points in lines:
+        x1,y1,x2,y2=points[0]
+        edges.append([(x1,y1), (x2,y2)])
+        segmentatedEdge = cv.cvtColor(segmentatedEdge, cv.COLOR_BGR2RGB)
+        cv.line(segmentatedEdge,(x1,y1),(x2,y2),(0,255,0),2)
+        cv.imwrite("111.png", segmentatedEdge)
+    #print(edges)
+
+    for edge in edges:
+        startPoint = -1
+        endPoint = -1
+        #print(f"{edge} \n")
+        for i in range(0, len(nodes)):
+            center_x = nodes[i][0][0]
+            center_y = nodes[i][0][1]
+            start_x = edge[0][0] - center_x
+            start_y = edge[0][1] - center_y
+            #print(i, center_x, center_y, edge[0][0], edge[0][1], start_x, start_y, start_x*start_x + start_y*start_y, nodes[i][1])
+            if start_x*start_x + start_y*start_y <= (nodes[i][1]+10)*(nodes[i][1]+10):
+                startPoint = i
+                break
+        for i in range(0, len(nodes)):
+            center_x = nodes[i][0][0]
+            center_y = nodes[i][0][1]
+            end_x = edge[1][0] - center_x
+            end_y = edge[1][1] - center_y
+            if end_x*end_x + end_y*end_y <= (nodes[i][1]+10)*(nodes[i][1]+10):
+                endPoint = i
+                break
+        if endPoint == -1 or startPoint == -1:
+            continue
+        matrix[startPoint][endPoint] = 1
+        matrix[endPoint][startPoint] = 1
+    print(matrix)
 
 def main():
     global parser
@@ -52,8 +104,6 @@ def main():
     image = preprocessing(image)
     image = segmentation(image)
     
-    
-
 
 if __name__ == "__main__":
     args()
